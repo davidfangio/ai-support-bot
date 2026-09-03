5b9cc7dddf63c53864b397b8651bdf786810cd4f
David Assunçao Lopes <dawaceo@gmail.com>
2026-09-03 17:25:51 -0300

    feat: configure chatbot environment

---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request 
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home ():
+    return "AI Support Bot Online."
+
+@main.route("/chat", methods=["POST"])
+def chat():
+    dados = request.json
+    if not dados or "mensagem" not in dados:
+        return "Robbie precisa de uma mensagem para responder.", 400
+
+    mensagem = dados["mensagem"]
+
+    return f"Robbie recebeu: {mensagem}"
+
+
+    
+
+
+
+            
